dd80688313f88ac26773a0e8e9acb0202232cce1
Prahr <wachter.ryan@gmail.com>
2021-12-18 14:10:37 -0500

    Initial commit

---
+++ sportbettingdashboard/src/main/java/sportbettingdashboard/SportbettingdashboardApplication.java
@@ -0,0 +1,13 @@
+package sportbettingdashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportbettingdashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportbettingdashboardApplication.class, args);
+	}
+
+}
---
+++ sportbettingdashboard/src/test/java/sportbettingdashboard/SportbettingdashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package sportbettingdashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportbettingdashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
